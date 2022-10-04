085b3303e90d2e920b37da0bafe6476489139929
Sangeeth Selvam <sangeethitselvam@gmail.com>
2022-10-04 21:03:48 +0530

    test

---
+++ common.js
@@ -0,0 +1,736 @@
+var x,check;
+var y;
+var text ={
+   "Final_Resp" :[
+  {
+    "position_number": "0_0", 
+    "storage_status": 2, 
+    "availability": false, 
+    "tray_info": {
+      "tray_number": "cdda1d326b6949538a583c84d915c177_2", 
+      "quantity": 35, 
+      "loaded_quantity": 35, 
+      "loading_date": "2022-01-02T18:49:29.000468", 
+      "unloading_date": "2022-01-30T18:49:33.753320", 
+      "status": "Loaded", 
+      "wish_date": "2021-12-30T00:00:00"
+    }
+  }, 
+  {
+    "position_number": "0_1", 
+    "storage_status": 1, 
+    "availability": false, 
+    "tray_info": {
+      "tray_number": "cdda1d326b6949538a583c84d915c177_1", 
+      "quantity": 35, 
+      "loaded_quantity": 35, 
+      "loading_date": "2022-01-02T18:39:29.000468", 
+      "unloading_date": "2022-01-30T18:39:33.753320", 
+      "status": "Loaded", 
+      "wish_date": "2021-12-30T00:00:00"
+    }
+  }, 
+  {
+    "position_number": "0_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "0_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "0_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "0_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "0_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "1_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "1_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "1_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "1_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "1_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "1_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "1_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "2_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "2_1", 
+    "storage_status": 1, 
+    "availability": false, 
+    "tray_info": {
+      "tray_number": "cdda1d326b6949538a583c84d915c177_3", 
+      "quantity": 35, 
+      "loaded_quantity": 35, 
+      "loading_date": "2022-01-02T17:59:29.000468", 
+      "unloading_date": "2022-01-30T17:59:33.753320", 
+      "status": "Loaded", 
+      "wish_date": "2021-12-30T00:00:00"
+    }
+  }, 
+  {
+    "position_number": "2_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "2_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "2_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "2_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "2_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "3_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "3_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "3_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "3_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "3_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "3_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "3_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "4_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "4_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "4_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "4_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "4_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "4_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "4_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "5_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "5_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "5_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "5_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "5_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "5_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "5_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "6_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "6_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "6_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "6_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "6_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "6_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "6_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "7_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "7_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "7_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "7_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "7_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "7_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "7_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "8_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "8_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "8_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "8_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "8_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "8_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "8_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "9_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "9_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "9_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "9_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "9_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "9_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "9_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "10_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "10_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "10_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "10_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "10_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "10_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "10_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "11_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "11_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "11_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "11_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "11_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "11_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "11_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "12_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "12_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "12_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "12_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "12_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "12_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "12_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "13_0", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "13_1", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "13_2", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "13_3", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "13_4", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "13_5", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }, 
+  {
+    "position_number": "13_6", 
+    "storage_status": 1, 
+    "availability": true, 
+    "tray_info": null
+  }
+]
+
+};
+
+
+var obj = JSON.parse(JSON.stringify(text));
+
+
+
+function myLoad() {
+
+    console.info(obj.Final_Resp[0].availability);
+    
+    for(x=1;x<=98;x++){
+        
+        check = document.getElementById("grid-item"+x).value;
+        console.info(check);
+        
+        for(y=1;y<=98;y++){
+           console.info(obj.Final_Resp[y-1].position_number);
+            if(check == obj.Final_Resp[y-1].position_number){
+                console.info(obj.Final_Resp[y-1].availability);
+                if(obj.Final_Resp[y-1].availability == false){
+                    document.getElementById("grid-item"+x).style.backgroundColor = "OrangeRed";
+                }
+                else if(obj.Final_Resp[y-1].availability == true){
+                    document.getElementById("grid-item"+x).style.backgroundColor = "LimeGreen";
+                }
+                break;
+            }
+        }
+    }
+    /*for(x=1;x<=98;x++){
+        if(obj.Final_Resp[x-1].availability == 'True'){
+           document.getElementById("grid-item"+x).style.backgroundColor = "OrangeRed";
+        }
+        else{
+            document.getElementById("grid-item"+x).style.backgroundColor = "LimeGreen";
+        }
+    }*/
+}
+
+
+
+function myFunction(e){
+
+    var rowContent,finalPrint;
+    console.info(e.value);
+    
+    for(x=1;x<=98;x++){
+        
+        if(e.value == obj.Final_Resp[x-1].position_number){
+            
+            if(obj.Final_Resp[x-1].availability == false){
+                
+                finalPrint= obj.Final_Resp[x-1].tray_info;
+                console.info(finalPrint);
+               
+                rowContent = "<tr><td><b>tray_number </b></td><td> : </td><td>"+finalPrint.tray_number+"</td></tr></br>"+
+                    "<tr><td><b>quantity </b></td><td> : </td><td>"+finalPrint.quantity+"</td></tr></br>" +
+                    "<tr><td><b>loaded_quantity </b></td><td> : </td><td>"+finalPrint.loaded_quantity+"</td></tr></br>" +
+                    "<tr><td><b>loading_date </b></td><td> : </td><td>"+finalPrint.loading_date+"</td></tr></br>" +
+                    "<tr><td><b>unloading_date </b></td><td> : </td><td>"+finalPrint.unloading_date+"</td></tr></br>" +
+                    "<tr><td><b>status </b></td><td> : </td><td>"+finalPrint.status+"</td></tr></br>" +
+                    "<tr><td><b>wish_date </b></td><td> : </td><td>"+finalPrint.wish_date+"</td></tr>";
+
+                Ext.Msg.show({
+                    title:'Trade Details',
+                    msg:rowContent,
+                    buttons:Ext.Msg.OK,
+                    minWidth:500,
+                    minHeight:300
+                });
+            }
+            else{
+                Ext.Msg.show({
+                    title:'Trade Details',
+                    msg:'No Tray Exists in this position',
+                    buttons:Ext.Msg.OK,
+                    minWidth:500,
+                    minHeight:300
+                });
+            }
+            break;
+        }
+    }
+    
+    /*if(obj.Final_Resp[e.value-1].availability == "True"){
+
+        console.info(obj.Final_Resp[e.value-1].tray_info[0].keys);
+
+        var finalPrint= obj.Final_Resp[e.value-1].tray_info[0];
+
+        rowContent = "<tr><td><b>tray_number </b></td><td> : </td><td>"+finalPrint.tray_number+"</td></tr></br>"+
+            "<tr><td><b>quantity </b></td><td> : </td><td>"+finalPrint.quantity+"</td></tr></br>" +
+            "<tr><td><b>loaded_quantity </b></td><td> : </td><td>"+finalPrint.loaded_quantity+"</td></tr></br>" +
+            "<tr><td><b>loading_date </b></td><td> : </td><td>"+finalPrint.loading_date+"</td></tr></br>" +
+            "<tr><td><b>unloading_date </b></td><td> : </td><td>"+finalPrint.unloading_date+"</td></tr>" ;
+
+         Ext.Msg.show({
+             title:'Trade Details',
+             msg:rowContent,
+             buttons:Ext.Msg.OK,
+             minWidth:500,
+             minHeight:300
+         });
+        //Ext.Msg.alert('Trade Details', rowContent);
+    }
+    else{
+        Ext.Msg.show({
+             title:'Trade Details',
+             msg:'No Tray Exists in this position',
+             buttons:Ext.Msg.OK,
+             minWidth:500,
+             minHeight:300
+         });
+        //Ext.Msg.alert('Trade Details', 'Empty Tray Information');
+    }*/
+
+}
